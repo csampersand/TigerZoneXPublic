3e09100c03a33a86f3da9fec17bb76d77a709c7b
hsiungwilliam <hsiung.william.ib@gmail.com>
2016-11-06 12:35:01 -0500

    Changed board size

--- Board.hpp
+++ Board.hpp
@@ -16,8 +16,9 @@ public:
     bool isPlacementValid(int, int, Tile*);
     bool placeTile(int, int, Tile*);
 private:
-    TileRelation* board[144][144];
+    TileRelation* board[145][145];
     TileRelation* firstTile;
+	//Make sure to start at 45,45
 };
 
 #endif /* Board_hpp */
