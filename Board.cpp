4f8d868c15a44b61924c8e223ff15082794a741c
hsiungwilliam <hsiung.william.ib@gmail.com>
2016-11-06 03:05:54 -0500

    Board coordinates and added unneccessary test.cpp and test.hpp lol
    lol

--- Board.cpp
+++ Board.cpp
@@ -18,7 +18,6 @@ bool Board::isPlacementValid(int x, int y, Tile* tile) {
 	// Either coordinate cant be > 144 or < 1
 	// Board configured so that edge will have an extra row and column to check
     if(x > 144 || x < 1 || y < 1 || y > 144) return false;    //Invalid Board coordinate
-
     if(board[x][y] != NULL) return false;  //Tile already placed in coordinate
 
     /*
