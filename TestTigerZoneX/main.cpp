296ca013a1b9aea012e324dbcc3c7b6dc0166db3
Chris Anderson <chris.scott.anders@gmail.com>
2016-11-11 01:19:51 -0500

    Add catch and set up new target

--- TestTigerZoneX/main.cpp
+++ TestTigerZoneX/main.cpp
@@ -5,5 +5,11 @@
 //  Created by Chris on 11/11/16.
 //  Copyright © 2016 Chris Anderson. All rights reserved.
 //
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <iostream>
 #include "catch.hpp"
+
+int main(int argc, const char * argv[]) {
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+    std::cout << "Hello, World!\n";
+    return 0;
--- TestTigerZoneX/testTile.cpp
+++ TestTigerZoneX/testTile.cpp
@@ -6,19 +6,4 @@
 //  Copyright © 2016 Chris Anderson. All rights reserved.
 //
 
-#include "catch.hpp"
-
-#include "Tile.hpp"
-
-TEST_CASE("Testing tile class") {
-    Tile t(Tile::sideCity, Tile::sideFarm, Tile::sideRoad, Tile::sideRoad, Tile::centerCity, false);
-    
-    SECTION("rotating the tile") {
-        INFO("Rotating 3 times")
-        t.rotate(3);
-        REQUIRE(t.getNType() == Tile::sideFarm);
-        REQUIRE(t.getEType() == Tile::sideRoad);
-        REQUIRE(t.getSType() == Tile::sideRoad);
-        REQUIRE(t.getWType() == Tile::sideCity);
-    }
-}
+#include <stdio.h>
