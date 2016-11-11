b0522d6fc19532ff744005f7f1294e07cf68df5c
hsiungwilliam <hsiung.william.ib@gmail.com>
2016-11-11 02:27:57 -0500

    Finished nextTurn and startGame methods
    # Conflicts:
    #	Game.cpp
    #	Game.hpp
    #	Player.cpp

--- Board.cpp
+++ Board.cpp
@@ -13,8 +13,6 @@ Board::Board() {
 }
 
 bool Board::isPlacementValid(int x, int y, Tile* tile) {
-    // TODO: Make sure this doesn't break things by checking tiles off the board
-
 	// Either coordinate cant be > 144 or < 1
 	// Board configured so that edge will have an extra row and column to check
     if(x > 144 || x < 1 || y < 1 || y > 144) return false;    //Invalid Board coordinate
