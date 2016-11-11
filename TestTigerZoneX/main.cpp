13833bff4cf4c1047f5d9eaf75310875baa5aca3
Chris Anderson <chris.scott.anders@gmail.com>
2016-11-11 02:09:02 -0500

    First test written for Tile rotation

--- TestTigerZoneX/main.cpp
+++ TestTigerZoneX/main.cpp
@@ -5,8 +5,7 @@
 //  Created by Chris on 11/11/16.
 //  Copyright © 2016 Chris Anderson. All rights reserved.
 //
-
-#include <iostream>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 int main(int argc, const char * argv[]) {
