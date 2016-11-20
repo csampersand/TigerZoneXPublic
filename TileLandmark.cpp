8c762a221ecae69a232cbf89618c906a53cf724a
Chris Anderson <chris.scott.anders@gmail.com>
2016-11-19 23:12:06 -0500

    Add TileLandmark class

---
+++ TileLandmark.cpp
@@ -0,0 +1,9 @@
+//
+//  TileLandmark.cpp
+//  TigerZoneX
+//
+//  Created by Chris on 11/19/16.
+//  Copyright © 2016 Chris Anderson. All rights reserved.
+//
+
+#include "TileLandmark.hpp"
---
+++ TileLandmark.hpp
@@ -0,0 +1,20 @@
+//
+//  TileLandmark.hpp
+//  TigerZoneX
+//
+//  Created by Chris on 11/19/16.
+//  Copyright © 2016 Chris Anderson. All rights reserved.
+//
+
+#ifndef TileLandmark_hpp
+#define TileLandmark_hpp
+
+#include <stdio.h>
+#include "Player.hpp"
+
+class TileLandmark {
+private:
+    Player* tigerOwnedBy;
+};
+
+#endif /* TileLandmark_hpp */
