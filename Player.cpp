eeb1499da6671d238ffe2a1f65523caa40751f81
Chris Anderson <chris.scott.anders@gmail.com>
2016-11-19 23:35:06 -0500

    Fix Player

--- Player.cpp
+++ Player.cpp
@@ -2,10 +2,6 @@
 
 Player::Player() {
 	this->score = 0;
-<<<<<<< HEAD
 	//8 meeples
 	this->followerCount = 8;
-=======
-	//TODO: Need to find out how many meeple does a play start out with in respect to how many players their are
->>>>>>> b0b627c... Added skeleton code for Game and Player objects
 }
