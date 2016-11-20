c777f2685e9d22e312bfded6af436ce02f28e010
Chris Anderson <chris.scott.anders@gmail.com>
2016-11-19 19:19:16 -0500

    Fix everything

--- TestTigerZoneX/main.cpp
+++ TestTigerZoneX/main.cpp
@@ -7,8 +7,3 @@
 //
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-
-int main(int argc, const char * argv[]) {
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-    std::cout << "Hello, World!\n";
-    return 0;
