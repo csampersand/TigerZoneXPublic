40050138e578a341306377b57640956a43107956
Chris Anderson <chris.scott.anders@gmail.com>
2016-11-27 23:22:32 -0500

    Move main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  TigerZoneX
+//
+//  Created by Chris on 11/4/16.
+//  Copyright © 2016 Chris Anderson. All rights reserved.
+//
+
+#include <iostream>
+#include "Game.hpp"
+
+int main(int argc, const char * argv[]) {
+    Game
+    
+    return 0;
+}
