84c3135c47a4a641a5430b3f36d6f8b7467e85be
connorelkin <celkin@ufl.edu>
2016-11-30 02:55:41 -0500

    DB

--- tests/testTile.cpp
+++ tests/testTile.cpp
@@ -13,6 +13,7 @@
 #include "Game.hpp"
 #include "TileDeck.hpp"
 #include "GameInteractor.hpp"
+#include <iostream>
 
 
 //class TestTileLandmark : public TileLandmark {
@@ -72,11 +73,154 @@ TEST_CASE("Testing Tile class") {
         REQUIRE(t->getEType() == Tile::sideTrail);
         REQUIRE(t->getSType() == Tile::sideTrail);
         REQUIRE(t->getWType() == Tile::sideLake);
+
+    }
+}
+
+TEST_CASE("Testing the Place Landmark function"){
+    Tile* tile1 = TestGameInteractor::createTileFromTemplate('E');
+    Tile* tile2 = TestGameInteractor::createTileFromTemplate('S');
+////    Tile* tile3 = TestGameInteractor::createTileFromTemplate('');
+////    Tile* tile4 = TestGameInteractor::createTileFromTemplate('E');
+//
+//
+    TestGameInteractor* tgi = new TestGameInteractor();
+    //76 76
+    tgi->placeTile(75,76, tile1);
+    tgi->placeTile(75,75, tile1);
+    tgi->placeTile(75,74, tile1);
+    tgi->placeTile(76,74, tile1);
+    tgi->placeTile(76,75, tile1);
+
+
+    SECTION("tile placed has connected two trails"){
+////        tgi->placeTile(72,74,tile2);
+        TileLandmark* lam = tgi->getGame()->getBoard()->getTileLandmark(76,76,8);
+//        std::cout << lam->getLandmarkType() <<std::endl;
+        TileTrail* trail;
+        trail = &static_cast<TileTrail&>(*lam);
+//        std::cout << "next: " << trail->getNextTrail() << std::endl;
+//        std::cout << "prev: " << trail->getPrevTrail() << std::endl;
+
+        if(trail->getNextTrail() != NULL){
+            trail = trail->getNextTrail()->getNextTrail();
+        }
+        else if(trail->getPrevTrail() != NULL){
+            trail = trail->getPrevTrail()->getPrevTrail();
+        }
+        else{
+            std::cout << "Both are null\n";
+        }
+//        trail = trail->getNextTrail()->getNextTrail();
+        TileLandmark* temp = tgi->getGame()->getBoard()->getTileLandmark(76,74,2);
+        TileTrail* trail2;
+        trail2 = &static_cast<TileTrail&>(*temp);
+
+//TEST_CASE("does the tile place"){
+//    TestGameInteractor* gameInt = new TestGameInteractor::TestGameInteractor();
+//
+//    Tile* tempTile = gameInt->drawTile();
+//    gameInt->placeTile(72,73,tempTile);
+//    Tile* placedTile = gameInt->getTilePointer(72, 73);
+//
+//
+//    SECTION("placing tile"){
+//        INFO("tile is being placed")
+//        REQUIRE(tempTile == placedTile);
+//
+//    }
+//
+//
+//}
+
+        INFO("connecting two seperate trails")
+        REQUIRE(trail==trail2);
+    }
+}
+////    SECTION("tilelandmark is not completed"){
+////        tgi->placeTile(72,74,tile2);
+////        INFO("tilelandmark was not bool completed")
+////        REQUIRE();
+////    }
+//}
+//
+//TEST_CASE("Testing Tile class") {
+//    Tile* t = new Tile::Tile(Tile::sideLake, Tile::sideJungle, Tile::sideTrail, Tile::sideTrail, Tile::centerLake);
+//
+//    SECTION("rotating the tile") {
+//        INFO("Rotating 3 times")
+//        GameInteractor::rotateTile(t,3);
+//        REQUIRE(t->getNType() == Tile::sideJungle);
+//        REQUIRE(t->getEType() == Tile::sideTrail);
+//        REQUIRE(t->getSType() == Tile::sideTrail);
+//        REQUIRE(t->getWType() == Tile::sideLake);
+//    }
+//}
+//
+//TEST_CASE("does it shuffle deck"){
+//    TestGameInteractor* gameInt = new TestGameInteractor::TestGameInteractor();
+//
+//
+//    SECTION("shuffling the deck"){
+//        INFO("deck is being shuffled")
+//        std::vector<Tile*> deck1 = gameInt->defineTiles();
+//        gameInt->getGame()->deck = deck1;
+//        //std::vector<Tile *> deck2 = deck1.shuffleDeck();
+//
+//        Tile* fTile = GameInteractor::createTileFromTemplate('F');
+//        bool one = (tile->getNType() != fTile->getNType());
+//
+//        bool two = (tile->getSType() != fTile->getSType());
+//        bool three = (tile->getWType() != fTile->getWType());
+//        bool four = (tile->getEType() != fTile->getEType());
+//        bool temp = one && two;
+//        bool temp2 = temp && three;
+//        bool temp4 = temp2 && four;
+//        REQUIRE(temp4);
+//
+//
+//
+//    }
+//}
+
+
+//TEST_CASE("does the tile place"){
+//    TestGameInteractor* gameInt = new TestGameInteractor::TestGameInteractor();
+//
+//    Tile* tempTile = gameInt->drawTile();
+//    gameInt->placeTile(72,73,tempTile);
+//    Tile* placedTile = gameInt->getTilePointer(72, 73);
+//
+//
+//    SECTION("placing tile"){
+//        INFO("tile is being placed")
+//        REQUIRE(tempTile == placedTile);
+//
+//    }
+//
+//
+//}
+
+TEST_CASE("Does the trails append?"){
+    SECTION("appending now"){
+        TestGameInteractor* testgi = new TestGameInteractor::TestGameInteractor();
+        //testgi
+        Tile* tile1 = TestGameInteractor::createTileFromTemplate('S');
+        //Tile* tile2 = TestGameInteractor::createTileFromTemplate('S');
+        testgi->placeTile(76,75, tile1);
+        //static_cast<TileTrail*>(TileLandmark)
+        TileLandmark* tl = testgi->getGame()->getBoard()->getLandmark(76,76,8);
+        TileLandmark* tl2 = testgi->getGame()->getBoard()->getLandmark(76,75,2);
+        INFO("Appended")
+//        REQUIRE(before->getNextTrail() == after);
+//        REQUIRE(after->getPrevTrail() == before);
+        REQUIRE(!testgi->isComplete(tl));
+
     }
 }
 
 //Testing has Owner
-TEST_CASE("Testing Tile class") {
+TEST_CASE("Testing isComplete and lakes") {
 
 	//Start a test game interactor
 	TestGameInteractor* gi = new TestGameInteractor();
@@ -109,6 +253,7 @@ TEST_CASE("Testing Tile class") {
 	gi->placeTile(79, 77, t7977);
 	gi->placeTile(79, 76, t7976);
 	gi->placeTile(79, 75, t7975);
+    //std::cout(gi->getGame()->getBoard()->tiles[78][76])
 
     SECTION("Layed some sweet tile") {
     	//grabbing tile landmark at 79,76 zone 2
