dd2e7a8590be0410f2f54546d7f5fee7ebe7fb01
Chris Anderson <chris.scott.anders@gmail.com>
2016-12-01 21:53:04 -0500

    Create bot interface

---
+++ src/interfaces/BotInterface.cpp
@@ -0,0 +1,9 @@
+//
+//  BotInterface.cpp
+//  TigerZoneX
+//
+//  Created by Chris on 12/1/16.
+//  Copyright © 2016 Chris Anderson. All rights reserved.
+//
+
+#include "BotInterface.hpp"
---
+++ src/interfaces/BotInterface.hpp
@@ -0,0 +1,14 @@
+//
+//  BotInterface.hpp
+//  TigerZoneX
+//
+//  Created by Chris on 12/1/16.
+//  Copyright © 2016 Chris Anderson. All rights reserved.
+//
+
+#ifndef BotInterface_hpp
+#define BotInterface_hpp
+
+#include <stdio.h>
+
+#endif /* BotInterface_hpp */
