c74ca0fbd8af5d5ac685cf7f7acaae8702a28d61
Jacob <jacobgonzalez731@gmail.com>
2016-12-02 00:10:16 -0500

    Added code to randomly pick returned valid moves

--- src/interfaces/BotInterface.cpp
+++ src/interfaces/BotInterface.cpp
@@ -8,11 +8,27 @@
 
 #include "BotInterface.hpp"
 #include <vector>
+#include <chrono>
+#include <random>
 
 void BotInterface::update()
 {
 	GameInteractor g = this->getInteractor();
-	std::vector<Move> moves = g.listPossibleMoves(
+	std::vector<Move> moves = g.listPossibleMoves();
+
+	typedef std::chrono::high_resolution_clock myclock;
+	myclock::time_point beginning = myclock::now();
+	myclock::duration d = myclock::now() - beginning;
+	unsigned seed = d.count();
+	auto engine = std::default_random_engine{};
+	engine.seed(seed);
+	std::shuffle(std::begin(moves), std::end(moves), engine);
+
 	//pick a random move
+	Move move = moves.front();
 	//play the move
+	if(move.tigerZone == 0)
+		g.playTurn(move.x, move.y, move.rotations, false, move.croc, move.tigerZone);
+	else
+		g.playTurn(move.x, move.y, move.rotations, true, move.croc, move.tigerZone);
 }
